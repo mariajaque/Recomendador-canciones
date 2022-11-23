5d423344eee427d0fe8a221e7ad3ce6c8415c133
Maria <202110874@alu.comillas.edu>
2022-11-23 16:47:23 +0100

    Version 3

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Recomendador-canciones
-En este repositorio se encuentra un recomendador de canciones en función de diversas categorías como el artista, el género, la popularidad y el año de lanzamiento
+En este repositorio se encuentra un recomendador de canciones en función de diversas categorías como el artista, el género, la popularidad y el año de lanzamiento.
+
+Para ello, el repositorio cuenta con los siguientes archivos necesarios para la ejecución:
+- **"requirements.txt"**: contiene un listado de todas las librerías necesarias para la correcta ejecución del código.
+- **"songs_normalize.csv"**: contiene información relativa a numerosos títulos de canciones como por ejemplo su autor o su popularidad.
+- **"recomendador.py"**: contiene el código necesario para poder recomendar. Emplea una estructura de ETL en la cuál primero extrae todos los valores del fichero ".csv", posteriormente los transforma hasta quedarse únicamente con aquellos que satisfacen los criterios de búsqueda y filtrado de canciones del usuario y finalmente los devuelve por pantalla.
+
+
+### Ejecución del Código:
+
+Primero debe hacerse un pip install requirements.txt para que se instalen en el dispositivo las librerías necesarías para la ejecución del código. Posteriormente ya podrá ejecutarse el fichero "recomendador.py". Al ejecutar el ".py" se solicita al usuario que introduzca el nombre de la categoría por la que desea realizar la recomendación. Una vez seleccionada una categoría válida se muestran los filtros posibles dentro de la misma (por ejemplo si se introduce la categoría "artist" se mostrarán todos los artistas que tienen canciones en el fichero ".csv"). Finalmente debe de introducirse el nombre de alguno de los filtros (por ejemplo el nombre de un autor) y el programa mostrará por pantalla todas las canciones que cumplan esos criterios de búsqueda.
--- recomendador.py
+++ recomendador.py
@@ -5,10 +5,17 @@ import sys
 
 
 class GeneroNoEncontrado(Exception):
+
+    # Definimos una clase excepción en caso de que el usuario
+    # introduzca un filtro que no se encuentre en el dataset
+
     print('\nNO ENCONTRADO')
 
 
 def handler_signal(signal,frame):
+
+    # Realizamos una salida controlada del programa en caso de que
+    # el usuario presione control C
 
     print("\n\n [!] out .......\ n")
 
@@ -18,14 +25,27 @@ signal.signal(signal.SIGINT,handler_signal)
 
 
 def extract():
+
+    # Se extraen los datos del dataset con las canciones
+
     df = pd.read_csv('songs_normalize.csv', sep=',')
     return df
 
 
 def transform(df):
 
+    # Se transforman para quedarse solo con aquellas canciones que cumplan
+    # los filtros del usuario
+
+    # Se dejará al usuario filtrar pos las categorías de artista, año
+    # género y popularidad
+
     categorias = ['artist', 'year', 'genre', 'popularity']
     cat = True
+
+    # Se pregunta al usuario por el nombre de la categoría de recomendación
+    # que desea hasta que introduzca una válida
+
     while cat:
 
         print('Se recomiendan canciones en base a las siguientes categorías')
@@ -35,24 +55,35 @@ def transform(df):
 
         entrada = input('¿Cuál desea? ')
 
+        # Se comprueba si el nombre de la categoría introducida por el usuario es
+        # válida.
+
         for i in categorias:
             if re.match(entrada, i, re.I) != None:
                 parametro = i
                 cat = False
 
+    # Se extraen las distintas posibilidades dentro de la categoría seleccionada
+
     posibilidades = df[parametro].unique()
 
     print('Posibilidades: ')
-    for i in posibilidades: print(i)
+    for i in posibilidades:
+        print(i)
     entrada = input('¿Cuál desea?: ')
 
     genero = []
 
+    # Se analiza el segundo filtro introducido por el usuario
     for i in posibilidades:
         regex = re.match(entrada, str(i), re.I)
 
         if regex:
             genero.append(i)
+
+    # En caso de que el segundo filtro sea válido se extraen
+    # todas las canciones que cumplan el filtro y se añaden a la lista
+    # de canciones
 
     if genero != []:
 
@@ -65,12 +96,19 @@ def transform(df):
             if df.loc[i, parametro] in genero:
                 canciones.append(df.song.loc[i])
 
+    # Si no se encuentra el filtro solicirado se muestra un mensaje de error
+    # por pantalla y se lanza una excepción
+
     else:
         raise GeneroNoEncontrado(f'No se encontró {entrada}')
+
+    # Se devuelven las canciones recomendadas
     return canciones
 
 
 def load(data):
+
+    # Se muestran las canciones recomendadas
 
     print()
     for song in data:
@@ -78,6 +116,7 @@ def load(data):
 
 
 if __name__ == '__main__':
+
     df = extract()
     try:
         data = transform(df)
